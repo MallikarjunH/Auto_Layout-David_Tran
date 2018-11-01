69b6a9db0ed82bdaaabadfc1c9f826f65594abb2
Mallikarjun Hanagandi <mallikarjun.h@ladybirdweb.com>
2018-11-01 12:46:43 +0530

    update
    * Added 1st sample autolayout example for UIView

---
+++ Practice 01 - Basic UIView Example/Practice 01 - Basic UIView Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice 01 - Basic UIView Example
+//
+//  Created by Mallikarjun on 01/11/18.
+//  Copyright © 2018 Ladybird Web Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
