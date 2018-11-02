fe8c76e41fb2e2411f6adbe447b5d7ee419f30e6
Mallikarjun Hanagandi <mallikarjun.h@ladybirdweb.com>
2018-11-02 14:48:52 +0530

    update

--- Example 03 - quotes-starter/Example 03 - quotes-starter/AppDelegate.swift
+++ Example 03 - quotes-starter/Example 03 - quotes-starter/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  Quotes
+//  Example 03 - quotes-starter
 //
-//  Created by Duc Tran on 4/29/16.
-//  Copyright © 2016 Developers Academy. All rights reserved.
+//  Created by Mallikarjun on 02/11/18.
+//  Copyright © 2018 Ladybird Web Solution. All rights reserved.
 //
 
 import UIKit
@@ -14,14 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
---
+++ Example 03 - quotes-starter/Example 03 - quotes-starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example 03 - quotes-starter
+//
+//  Created by Mallikarjun on 02/11/18.
+//  Copyright © 2018 Ladybird Web Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
